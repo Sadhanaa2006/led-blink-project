a3026ae6d4889f53131312dc1888188507199ba8
Sadhana S <sadhanasgin07@gmail.com>
2026-06-03 18:45:51 +0530

    Update led.c

--- led.c
+++ led.c
@@ -1,35 +1,45 @@
 #include <reg51.h>
-sbit led1=P1^0;
-sbit led2=P1^1;
-sbit led3=P1^2;
-sbit led4=P1^3;
-sbit led5=P2^0;
-sbit led6=P2^1;
-sbit led7=P2^2;
-sbit led8=P2^3;
+
+sbit led1 = P1^0;
+sbit led2 = P1^1;
+sbit led3 = P1^2;
+sbit led4 = P1^3;
+sbit led5 = P2^0;
+sbit led6 = P2^1;
+sbit led7 = P2^2;
+sbit led8 = P2^3;
+
+void delay()
+{
+    unsigned int i, j;
+    for(i = 0; i < 500; i++)
+        for(j = 0; j < 500; j++);
+}
+
 void main()
 {
-unsigned int i;
-P1=0x00; P2=0x00;
-while(1)
-{
-led1=0xff;
-led2=0x00;
-led3=0xff;
-led4=0x00;
-led5=0xff;
-led6=0x00;
-led7=0xff;
-led8=0x00;
-for(i=0;i<60000;i++);
-led1=0x00;
-led2=0xff;
-led3=0x00;
-led4=0xff;
-led5=0x00;
-led6=0xff;
-led7=0x00;
-led8=0xff;
-for(i=0;i<60000;i++);
-}
+    while(1)
+    {
+        led1 = 1;
+        led2 = 0;
+        led3 = 1;
+        led4 = 0;
+        led5 = 1;
+        led6 = 0;
+        led7 = 1;
+        led8 = 0;
+
+        delay();
+
+        led1 = 0;
+        led2 = 1;
+        led3 = 0;
+        led4 = 1;
+        led5 = 0;
+        led6 = 1;
+        led7 = 0;
+        led8 = 1;
+
+        delay();
+    }
 }
